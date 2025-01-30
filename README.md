4b172c6afb5125d97c3e0d07f0162cc1feb0efa5
Dave Pearson <davep@davep.org>
2025-01-30 11:07:32 +0000

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# textual-enhanced
+
+## Introduction
+
+This library is a mildly-opinionated set of enhancements and extras for the
+Textual framework, mainly aimed at how I like my own Textual apps to look
+and work.
+
+[//]: # (README.md ends here)
---
+++ src/textual_enhanced/commands/__init__.py
@@ -0,0 +1,12 @@
+"""Provides code related to 'commands' in a Textual application."""
+
+##############################################################################
+# Local imports.
+from .command import Command
+from .provider import CommandsProvider
+
+##############################################################################
+# Exports.
+__all__ = ["Command", "CommandsProvider"]
+
+### __init__.py ends here
