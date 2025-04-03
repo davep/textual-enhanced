aeb08be9c31d6e49eb6108fcb5944a81399c91c5
Dave Pearson <davep@davep.org>
2025-04-03 14:59:07 +0100

    :hammer: Format the help so that it'll work in many Pythons

--- src/textual_enhanced/__main__.py
+++ src/textual_enhanced/__main__.py
@@ -68,7 +68,9 @@ class Main(EnhancedScreen[None]):
         ),
         HelpfulBinding("ctrl+t, ctrl+k", "gndn", description="This is the description"),
     )
-    HELP = f"""Here's some really lone text to scroll: {"\n - ".join([str(n) for n in range(100)])}"""
+    HELP = "Here's some really long text to scroll: {}".format(
+        "\n".join([f"{n}. This is a filler line" for n in range(100)])
+    )
 
     def compose(self) -> ComposeResult:
         yield Header()
